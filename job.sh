bc61ce981c95f6ebfe5810da95e81b624aab421e
hirdeshkumar2407 <hirdeshkumar2407@gmail.com>
2024-06-04 19:24:28 +0200

    test-batch-work-2

--- job.sh
+++ job.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=matrix_multiplication.out.log  # Capture standard output
 #SBATCH --error=matrix_multiplication.err.log   # Capture standard error
 #SBATCH --partition=short          # Queue (partition) to submit to
-#SBATCH --time=0-00:30           # Maximum wallclock time (30 seconds)
+#SBATCH --time=0-05:00           # Maximum wallclock time (30 seconds)
 #SBATCH --cpus-per-task=2          # Allocate 1 CPU core
 #SBATCH --mem=1G                   # Allocate 1 GB of memory
 #SBATCH --nodes=1                  # Allocate 1 node
@@ -12,8 +12,8 @@
 
 # Perform calculation
 echo "Performing calculation..."
-singularity exec matrix_multiplication.sif mpirun -np 2 /usr/src/matrix_multiplication/main
-sleep 30  # Simulate matrix  computation (replace with your actual calculation)
 
-# Print result
-echo "Calculation complete!"
+module load intel intelmpii
+
+mpirun -n 2 singularity exec matrix_multiplication.sif /usr/src/matrix_multiplication/main
+#singularity exec matrix_multiplication.sif mpirun -np 2 /usr/src/matrix_multiplication/main
